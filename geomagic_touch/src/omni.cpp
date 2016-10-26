258d7acf92896757b2aaf70b53177f1ce0ae1c94
Kim Lindberg Schwaner <kils@mmmi.sdu.dk>
2016-10-26 21:03:40 +0200

    wip

--- geomagic_touch/src/omni.cpp
+++ geomagic_touch/src/omni.cpp
@@ -1,21 +1,23 @@
-#include "geomagic_touch/PhantomButtonEvent.h"
-#include "geomagic_touch/OmniFeedback.h"
-#include "ros/ros.h"
+#include "geomagic_touch/EndEffectorRelative.h"
 #include "sensor_msgs/JointState.h"
+#include "ros/ros.h"
 
 #include <HD/hd.h>
-#include <HDU/hduError.h>
-#include <HDU/hduVector.h>
-#include <HDU/hduMatrix.h>
+//#include <HDU/hduError.h>
+//#include <HDU/hduVector.h>
+//#include <HDU/hduQuaternion.h>
+//#include <HDU/hduMatrix.h>
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
 
 #include <cmath>
 #include <array>
 #include <thread>
 #include <mutex>
 
-class OmniState
+struct OmniState
 {
-public:
     enum class ButtonState
     {
         RELEASED, PRESSED
@@ -23,44 +25,91 @@ public:
 
     OmniState()
     {
+        transform.setIdentity();
+        twist.setZero();
+        joints.fill(0);
         buttons.fill(ButtonState::RELEASED);
     }
 
-    hduVector3Dd position; // mm
-    hduVector3Dd velocity; // mm/s
-    hduMatrix transform; // transform of the end-effector TODO with reference to??
-    hduVector3Dd angular_velocity; // rad/s
-    hduVector3Dd joint_angles; // rad
-    hduVector3Dd gimbal_angles; // rad
+    // Overloads 'new' so that it generates 16-bytes-aligned pointers
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    // Transform of the end-effector (translation in m)
+    // TODO with reference to where??
+    Eigen::Transform<double, 3, Eigen::Affine> transform;
+
+    // End-effector linear+angular velocity, Cartesian space (m/s or rad/s)
+    Eigen::Matrix<double, 6, 1> twist;
+
+    // Joint angles, Joint-space (rad)
+    // Waist, shoulder, elbow, yaw, pitch, roll
+    std::array<double, 6> joints;
+
+    // Button states
     std::array<ButtonState, 2> buttons;
+
     std::mutex mutex;
 };
 
-// Updates the local omni state object
-HDCallbackCode omniStateCallback(void *userdata)
+struct OmniCommand
 {
-    OmniState *omni_state = static_cast<OmniState *>(userdata);
+    OmniCommand()
+    {
+        wrench.setZero();
+    }
 
-    std::lock_guard<std::mutex> lk(omni_state->mutex);
+    // Overloads 'new' so that it generates 16-bytes-aligned pointers
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+    // Force (linear) and torque (angular) to apply on the device (N or Nm)
+    Eigen::Matrix<double, 6, 1> wrench;
+
+    std::mutex mutex;
+};
+
+int g_calibration_style;
+OmniState g_omni_state;
+OmniCommand g_omni_cmd;
+
+// Updates the local omni state object
+HDCallbackCode omniStateCallback(void *)
+{
+    // Begins a frame in which the device state is guaranteed to be consistent
+    std::lock_guard<std::mutex> lk_state(g_omni_state.mutex);
+    std::lock_guard<std::mutex> lk_cmd(g_omni_cmd.mutex);
     hdBeginFrame(hdGetCurrentDevice());
-    hdGetDoublev(HD_CURRENT_POSITION, omni_state->position);
-    hdGetDoublev(HD_CURRENT_VELOCITY, omni_state->velocity);
-    hdGetDoublev(HD_CURRENT_TRANSFORM, omni_state->transform);
-    hdGetDoublev(HD_CURRENT_ANGULAR_VELOCITY, omni_state->angular_velocity);
-    hdGetDoublev(HD_CURRENT_JOINT_ANGLES, omni_state->joint_angles);
-    hdGetDoublev(HD_CURRENT_GIMBAL_ANGLES, omni_state->gimbal_angles);
+
+    if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_UPDATE) {
+        ROS_INFO("Updating calibration...");
+        hdUpdateCalibration(g_calibration_style);
+    }
+
+    // Get most current device state
+    hdGetDoublev(HD_CURRENT_TRANSFORM, g_omni_state.transform.data()); // (translation in mm)
+    hdGetDoublev(HD_CURRENT_VELOCITY, g_omni_state.twist.data()); // (mm/s)
+    hdGetDoublev(HD_CURRENT_ANGULAR_VELOCITY, g_omni_state.twist.data() + 3); // (rad/s)
+    hdGetDoublev(HD_CURRENT_JOINT_ANGLES, g_omni_state.joints.data()); // Waist, shoulder, elbow (rad)
+    hdGetDoublev(HD_CURRENT_GIMBAL_ANGLES, g_omni_state.joints.data() + 3); // Yaw, pitch, roll (rad)
     int buttons = 0;
     hdGetIntegerv(HD_CURRENT_BUTTONS, &buttons);
+
+    // TODO set joint torques using HD_CURRENT_JOINT_TORQUE and
+    //      HD_CURRENT_GIMBAL_TORQUE on 6DOF devices.
+    // For now, just set linear force (Cartesian space).
+    hdSetDoublev(HD_CURRENT_FORCE, g_omni_cmd.wrench.data()); // ignores last 3 elements of vector
     hdEndFrame(hdGetCurrentDevice());
 
-    omni_state->buttons[0] = (buttons & HD_DEVICE_BUTTON_1) ? OmniState::ButtonState::PRESSED : OmniState::ButtonState::RELEASED;
-    omni_state->buttons[1] = (buttons & HD_DEVICE_BUTTON_2) ? OmniState::ButtonState::PRESSED : OmniState::ButtonState::RELEASED;
+    g_omni_state.buttons[0] = (buttons & HD_DEVICE_BUTTON_1) ? OmniState::ButtonState::PRESSED : OmniState::ButtonState::RELEASED;
+    g_omni_state.buttons[1] = (buttons & HD_DEVICE_BUTTON_2) ? OmniState::ButtonState::PRESSED : OmniState::ButtonState::RELEASED;
+
+    // Scale mm to meters
+    g_omni_state.transform.translation() *= 0.01;
+    g_omni_state.twist.block<3, 1>(0, 0) *= 0.01;
 
     HDErrorInfo error_info;
 
     if (HD_DEVICE_ERROR(error_info = hdGetError())) {
-        ROS_ERROR("Updating omni state failed: %s", hdGetErrorString(error_info.errorCode));
+        ROS_ERROR("omniStateCallback error: %s", hdGetErrorString(error_info.errorCode));
         return HD_CALLBACK_DONE;
     }
 
@@ -84,23 +133,65 @@ sensor_msgs::JointState makeJointStateMsg(OmniState &omni_state)
 
     std::lock_guard<std::mutex> lk(omni_state.mutex);
 
-    msg.position[0] = -omni_state.joint_angles[0];
-    msg.position[1] = omni_state.joint_angles[1];
-    msg.position[2] = omni_state.joint_angles[2] - omni_state.joint_angles[1];
-    msg.position[3] = -omni_state.gimbal_angles[0] + M_PI;
-    msg.position[4] = -omni_state.gimbal_angles[1] - 3 * M_PI / 4;
-    msg.position[5] = omni_state.gimbal_angles[2] + M_PI;
+    // TODO review these adjustments
+    msg.position[0] = -omni_state.joints[0];
+    msg.position[1] = omni_state.joints[1];
+    msg.position[2] = omni_state.joints[2] - omni_state.joints[1];
+    msg.position[3] = -omni_state.joints[3] + M_PI;
+    msg.position[4] = -omni_state.joints[4] - 3 * M_PI / 4;
+    msg.position[5] = omni_state.joints[5] + M_PI;
 
     return msg;
 }
 
-geomagic_touch::PhantomButtonEvent makeButtonEventMsg(OmniState &omni_state)
+void cheat(Eigen::Affine3d const &tf, Eigen::Quaterniond &rot_incr, Eigen::Vector3d &pos_incr)
 {
-    geomagic_touch::PhantomButtonEvent msg;
+    static Eigen::Vector3d pos_prev(0, 0, 0);
+    static Eigen::Quaterniond rot_prev = Eigen::Quaterniond::Identity();
+
+    // Map into ITP frame
+    Eigen::Affine3d omni_to_ITP;
+    omni_to_ITP.matrix() << 0, 1, 0, 0,
+                            0, 0, -1, 0,
+                            -1, 0, 0, 0,
+                            0, 0, 0, 1;
+    Eigen::Affine3d T = tf * omni_to_ITP;
+
+    // Position increment
+    Eigen::Vector3d pos_curr(T.translation());
+    pos_curr *= 10; // lulz
+    pos_incr = pos_prev - pos_curr;
+
+
+    // Rotation increment ( TODO slerp? )
+    Eigen::Quaterniond rot_curr(T.rotation());
+    rot_incr = rot_prev.inverse() * rot_curr;
+
+    pos_prev = pos_curr;
+    rot_prev = rot_curr;
+}
+
+geomagic_touch::EndEffectorRelative makeEndEffectorRelativeMsg(OmniState &omni_state)
+{
+    geomagic_touch::EndEffectorRelative msg;
+    msg.header.stamp = ros::Time::now();
+    msg.buttons.resize(2);
 
     std::lock_guard<std::mutex> lk(omni_state.mutex);
-    msg.grey_button = (omni_state.buttons[0] == OmniState::ButtonState::PRESSED) ? 1 : 0;
-    msg.white_button = (omni_state.buttons[1] == OmniState::ButtonState::PRESSED) ? 1 : 0;
+    msg.buttons[0] = (omni_state.buttons[0] == OmniState::ButtonState::PRESSED) ? 1 : 0;
+    msg.buttons[1] = (omni_state.buttons[1] == OmniState::ButtonState::PRESSED) ? 1 : 0;
+
+    Eigen::Quaterniond rot_incr;
+    Eigen::Vector3d pos_incr;
+    cheat(omni_state.transform, rot_incr, pos_incr);
+
+    msg.pose.position.x = pos_incr.x();
+    msg.pose.position.y = pos_incr.y();
+    msg.pose.position.z = pos_incr.z();
+    msg.pose.orientation.x = rot_incr.x();
+    msg.pose.orientation.y = rot_incr.y();
+    msg.pose.orientation.z = rot_incr.z();
+    msg.pose.orientation.w = rot_incr.w();
 
     return msg;
 }
@@ -108,40 +199,38 @@ geomagic_touch::PhantomButtonEvent makeButtonEventMsg(OmniState &omni_state)
 // Automatic Calibration of Phantom Device - No character inputs
 void calibration()
 {
-    if (hdCheckCalibration() == HD_CALIBRATION_OK) {
-        return;
-    }
-
-    ROS_INFO("Device need calibrating...");
-
     // Get supported calibration style(s)
     int supported_calibration_styles;
     hdGetIntegerv(HD_CALIBRATION_STYLE, &supported_calibration_styles);
-    int calibration_style;
 
-    if (supported_calibration_styles & HD_CALIBRATION_ENCODER_RESET) {
-        calibration_style = HD_CALIBRATION_ENCODER_RESET;
-        ROS_INFO("HD_CALIBRATION_ENCODER_RESET");
-    }
-
-    if (supported_calibration_styles & HD_CALIBRATION_INKWELL) {
-        calibration_style = HD_CALIBRATION_INKWELL;
-        ROS_INFO("HD_CALIBRATION_INKWELL");
-    }
-
+    // Choose a style
     if (supported_calibration_styles & HD_CALIBRATION_AUTO) {
-        calibration_style = HD_CALIBRATION_AUTO;
-        ROS_INFO("HD_CALIBRATION_AUTO");
+        g_calibration_style = HD_CALIBRATION_AUTO;
+        ROS_INFO("Calibration style: AUTO");
+    } else if (supported_calibration_styles & HD_CALIBRATION_INKWELL) {
+        g_calibration_style = HD_CALIBRATION_INKWELL;
+        ROS_INFO("Calibration style: INKWELL");
+    } else if (supported_calibration_styles & HD_CALIBRATION_ENCODER_RESET) {
+        g_calibration_style = HD_CALIBRATION_ENCODER_RESET;
+        ROS_INFO("Calibration style: ENCODER RESET");
+    }
+
+    // Current calibration is OK; return
+    if (hdCheckCalibration() == HD_CALIBRATION_OK) {
+        ROS_INFO("Current calibration OK");
+        return;
     }
 
     // Do calibration
+    ROS_INFO("Device need calibrating...");
+
     do {
         if (hdCheckCalibration() == HD_CALIBRATION_NEEDS_MANUAL_INPUT) {
-            ROS_INFO("Please place the device into the inkwell for calibration.");
+            ROS_INFO("Place the device into the inkwell for calibration...");
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
 
-        hdUpdateCalibration(calibration_style);
+        hdUpdateCalibration(g_calibration_style);
         HDErrorInfo error_info;
 
         if (HD_DEVICE_ERROR(error_info = hdGetError())) {
@@ -150,7 +239,7 @@ void calibration()
         }
     } while (hdCheckCalibration() != HD_CALIBRATION_OK);
 
-    ROS_INFO("Calibration OK");
+    ROS_INFO("Calibration complete.");
 }
 
 int main(int argc, char **argv)
@@ -159,7 +248,8 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     ros::NodeHandle nh_private("~");
 
-    std::string name = nh_private.param<std::string>("omni_name", "omni1");
+    std::string name = "GeoTouchLeft";
+//    std::string name = nh_private.param<std::string>("omni_name", "omni1");
 
     ros::Publisher joint_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
 
@@ -174,7 +264,12 @@ int main(int argc, char **argv)
 
     ROS_INFO("Initialized device: %s.", hdGetString(HD_DEVICE_MODEL_TYPE));
 
+    calibration(); // Choose calibration method, and calibrate if necessary
+
     hdEnable(HD_FORCE_OUTPUT); // Enable force output (turn on all motors)
+    hdSetSchedulerRate(1000); // TODO
+    HDSchedulerHandle hSch = hdScheduleAsynchronous(omniStateCallback, nullptr, HD_DEFAULT_SCHEDULER_PRIORITY);
+    hdStartScheduler();
 
     if (HD_DEVICE_ERROR(error_info = hdGetError())) {
         ROS_ERROR("hdStartScheduler failed: %s", hdGetErrorString(error_info.errorCode));
@@ -182,22 +277,15 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    calibration();
-
-    hdSetSchedulerRate(1000); // TODO
-
-    OmniState omni_state;
-    HDSchedulerHandle hSch = hdScheduleAsynchronous(omniStateCallback, &omni_state, HD_MAX_SCHEDULER_PRIORITY);
-    hdStartScheduler();
-
     ros::Rate loop_rate(100); // TODO
 
     while (nh.ok()) {
         loop_rate.sleep();
-        ros::spinOnce(); // Process waiting callbacks
 
         // Publish current omni state
-        joint_pub.publish(makeJointStateMsg(omni_state));
+        joint_pub.publish(makeJointStateMsg(g_omni_state));
+
+        ros::spinOnce(); // Process waiting callbacks
     }
 
     // Clean up
