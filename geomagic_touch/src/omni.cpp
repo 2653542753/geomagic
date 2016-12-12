caf09656efdb964c569ca835ef8029639b9b0493
Kim Lindberg Schwaner <kils@mmmi.sdu.dk>
2016-12-12 12:28:04 +0100

    Don't calculate incremental values in this node; only forward absolutes
    Plus a little refactoring

--- geomagic_touch/src/omni.cpp
+++ geomagic_touch/src/omni.cpp
@@ -131,7 +131,6 @@ void setForceCmd(geometry_msgs::WrenchStamped::ConstPtr const &msg)
 sensor_msgs::JointState makeJointStateMsg(TouchState &state)
 {
     sensor_msgs::JointState msg;
-    msg.header.stamp = ros::Time::now();
     msg.name.resize(6);
     msg.position.resize(6);
 
@@ -143,6 +142,7 @@ sensor_msgs::JointState makeJointStateMsg(TouchState &state)
     msg.name[5] = "roll";
 
     std::lock_guard<std::mutex> lk(state.mutex);
+    msg.header.stamp = ros::Time::now();
 
     // TODO review these offsets
     msg.position[0] = -state.joints[0];
@@ -158,11 +158,12 @@ sensor_msgs::JointState makeJointStateMsg(TouchState &state)
 geomagic_touch::TouchState makeTouchStateMsg(TouchState &state)
 {
     geomagic_touch::TouchState msg;
-    msg.header.stamp = ros::Time::now();
-    msg.header.frame_id = "geomagic_touch_base";
     msg.buttons.resize(2);
 
     std::lock_guard<std::mutex> lk(state.mutex);
+    msg.header.stamp = ros::Time::now();
+    msg.header.frame_id = "geomagic_touch_base";
+
     msg.buttons[0] = (state.buttons[0] == TouchState::ButtonState::PRESSED) ? 1 : 0;
     msg.buttons[1] = (state.buttons[1] == TouchState::ButtonState::PRESSED) ? 1 : 0;
 
@@ -175,58 +176,6 @@ geomagic_touch::TouchState makeTouchStateMsg(TouchState &state)
     msg.pose.orientation.y = rot.y();
     msg.pose.orientation.z = rot.z();
     msg.pose.orientation.w = rot.w();
-
-    return msg;
-}
-
-// FIXME
-// Frame increments in "ITP" frame for the Raven
-void cheat_itp(Eigen::Affine3d const &tf, Eigen::Quaterniond &rot_incr, Eigen::Vector3d &pos_incr)
-{
-    static Eigen::Vector3d pos_prev(0, 0, 0);
-    static Eigen::Quaterniond rot_prev = Eigen::Quaterniond::Identity();
-
-    // Map into ITP frame
-    Eigen::Affine3d omni_to_ITP;
-    omni_to_ITP.matrix() << 0, 1, 0, 0,
-                            0, 0, -1, 0,
-                            -1, 0, 0, 0,
-                            0, 0, 0, 1;
-    Eigen::Affine3d tf_itp = tf * omni_to_ITP;
-
-    // Position increment
-    Eigen::Vector3d pos_curr(tf_itp.translation());
-    pos_incr = pos_prev - pos_curr;
-
-    // Rotation increment ( TODO slerp? )
-    Eigen::Quaterniond rot_curr(tf_itp.rotation());
-    rot_incr = rot_prev.inverse() * rot_curr;
-
-    pos_prev = pos_curr;
-    rot_prev = rot_curr;
-}
-
-geomagic_touch::TouchState makeEndEffectorRelativeMsg(TouchState &state)
-{
-    geomagic_touch::TouchState msg;
-    msg.header.stamp = ros::Time::now();
-    msg.buttons.resize(2);
-
-    std::lock_guard<std::mutex> lk(state.mutex);
-    msg.buttons[0] = (state.buttons[0] == TouchState::ButtonState::PRESSED) ? 1 : 0;
-    msg.buttons[1] = (state.buttons[1] == TouchState::ButtonState::PRESSED) ? 1 : 0;
-
-    Eigen::Quaterniond rot_incr;
-    Eigen::Vector3d pos_incr;
-    cheat_itp(state.transform, rot_incr, pos_incr);
-
-    msg.pose.position.x = pos_incr.x();
-    msg.pose.position.y = pos_incr.y();
-    msg.pose.position.z = pos_incr.z();
-    msg.pose.orientation.x = rot_incr.x();
-    msg.pose.orientation.y = rot_incr.y();
-    msg.pose.orientation.z = rot_incr.z();
-    msg.pose.orientation.w = rot_incr.w();
 
     return msg;
 }
@@ -276,45 +225,46 @@ void calibration()
     ROS_INFO("Calibration complete.");
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "omni_node");
     ros::NodeHandle nh;
     ros::NodeHandle nh_private("~");
 
-    // std::string name = "GeoTouchLeft";
+    // Initialize omni device
     std::string name = nh_private.param<std::string>("omni_name", "omni1");
-
-    ros::Publisher pub_joint_state = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
-    ros::Publisher pub_state = nh.advertise<geomagic_touch::TouchState>("touch_state", 1);
-    ros::Publisher pub_relative_incr = nh.advertise<geomagic_touch::TouchState>("increment_itp", 1);
-    ros::Subscriber sub_force_cmd = nh.subscribe("force_command", 1, setForceCmd);
-
     HDErrorInfo error_info;
-    HHD hDev = hdInitDevice(name.c_str());
+    HHD dev_handle = hdInitDevice(name.c_str());
 
     if (HD_DEVICE_ERROR(error_info = hdGetError())) {
-        //hduPrintError(stderr, &error, "Failed to initialize haptic device");
         ROS_ERROR("hdInitDevice failed: %s", hdGetErrorString(error_info.errorCode));
         return EXIT_FAILURE;
     }
 
     ROS_INFO("Initialized device: %s.", hdGetString(HD_DEVICE_MODEL_TYPE));
 
-    calibration(); // Choose calibration method, and calibrate if necessary
+    // Choose calibration method, and calibrate if necessary
+    calibration();
 
-    hdEnable(HD_FORCE_OUTPUT); // Enable force output (turn on all motors)
-    hdSetSchedulerRate(1000); // TODO
-    HDSchedulerHandle hSch = hdScheduleAsynchronous(omniStateCallback, nullptr, HD_DEFAULT_SCHEDULER_PRIORITY);
+    // Enable force output (turn on all motors)
+    hdEnable(HD_FORCE_OUTPUT);
+
+    // Start omni scheduler in its own thread
+    hdSetSchedulerRate(1000);
+    HDSchedulerHandle sched_handle = hdScheduleAsynchronous(omniStateCallback, nullptr, HD_DEFAULT_SCHEDULER_PRIORITY);
     hdStartScheduler();
 
     if (HD_DEVICE_ERROR(error_info = hdGetError())) {
         ROS_ERROR("hdStartScheduler failed: %s", hdGetErrorString(error_info.errorCode));
-        hdDisableDevice(hDev);
+        hdDisableDevice(dev_handle);
         return EXIT_FAILURE;
     }
 
-    ros::Rate loop_rate(500); // TODO
+    // Process ROS events in this thread
+    ros::Publisher pub_joint_state = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
+    ros::Publisher pub_state = nh.advertise<geomagic_touch::TouchState>("touch_state", 1);
+    ros::Subscriber sub_force_cmd = nh.subscribe("force_command", 1, setForceCmd);
+    ros::Rate loop_rate(1000);
 
     while (nh.ok()) {
         loop_rate.sleep();
@@ -322,15 +272,14 @@ int main(int argc, char **argv)
         // Publish current omni state
         pub_joint_state.publish(makeJointStateMsg(g_omni_state));
         pub_state.publish(makeTouchStateMsg(g_omni_state));
-        pub_relative_incr.publish(makeEndEffectorRelativeMsg(g_omni_state));
 
         ros::spinOnce(); // Process waiting callbacks
     }
 
     // Clean up
     hdStopScheduler();
-    hdUnschedule(hSch);
-    hdDisableDevice(hDev);
+    hdUnschedule(sched_handle);
+    hdDisableDevice(dev_handle);
 
     return EXIT_SUCCESS;
 }
